6a352d29fdc8a266e521594b4da523a449bacb66
michele <michele.braidotti@gmail.com>
2013-05-18 02:03:47 -0700

    greplist_scelta_colonna

---
+++ rosa/greplist_scelta_colonna.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+#Dati un file di input e un file lista, estraggo dall'input tutte le righe che contengono quelle parole
+#per es. data una lista di id estraggo dal file input tutte le linee che contengono quegli id
+
+#Uso:
+#perl greplist.pl lista.txt "file input"
+
+
+use strict;
+use warnings;
+
+my $list_f = $ARGV[0];
+my $search_f = $ARGV[1];
+my $searching_col = $ARGV[2];
+
+my $data;
+open(S, $search_f);
+while (my $line = <S>) {
+	$line =~ s/^\s+//;
+	$line =~ s/\s+$//;
+	if ( $line !~ /^$/ ) {
+		my @line = split(/\s+/, $line);
+		push(@{$data->{$line[$searching_col]}}, \@line);
+	}
+}
+close(S);
+
+open(F, $list_f);
+while (my $line = <F>) {
+	$line =~ s/^\s+//;
+	$line =~ s/\s+$//;
+	if ( $line !~ /^$/ ) {
+		if ( exists $data->{$line} ) {
+			foreach my $search_line (@{ $data->{$line}}) {
+				print join("\t", @$search_line);
+				print "\n";
+			}
+		}
+	}
+}
+close(F);
