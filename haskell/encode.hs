def150d951b2b32f6d20f685e95613cd11705353
michele <michele.braidotti@gmail.com>
2013-05-28 13:10:15 -0700

    more haskell madness

--- haskell/encode.hs
+++ haskell/encode.hs
@@ -1,6 +1,23 @@
 encode :: (Num b) => [Char] -> [(Char, b)]
 encode' :: (Num b) => [Char] -> [(Char, b)] -> [(Char, b)]
+increment :: Int -> (Char, Int) -> (Char, Int)
+
+increment x (k, v) = (k, v + x)
 
 encode [] = [('?', 0)]
-encode (x:xs) = encode' xs [(x, 1)]
-encode' (x:xs) y = encode' xs map (\x (k, v) -> if ( x == k ) then (k, v + 1) else (k, v)) y
+encode (x:xs) = encode' x [(x, 1)]  
+
+encode' (x:y:[]) res = 
+	if ( x /= y ) 
+	then init(res) ++ increment(1, tail(res)) ++ (y, 1)
+	else init(res) ++ increment(2, tail(res))
+encode' (x:y:xs) res = 
+	if ( x /= y )
+	then encode' (y:xs) (init(res) ++ increment(1, tail(res)) ++ (y, 1))
+	else encode' (y:xs) ( init(res) ++ increment(2, tail(res)))
+
+--foldl (\(k, v) x -> if ( x == k ) then (k, v + 1) else (k, v)) ("a", 0) "aaabbbb"
+
+
+--encode' [] y = y
+--encode' (x:xs) y = encode' xs (map (\(k, v) -> if ( x == k ) then (k, v + 1) else (k, v)) y)
---
+++ haskell/increment.hs
@@ -0,0 +1,5 @@
+increment :: Int -> (Char, Int) -> (Char, Int)
+
+increment x (k, v) = (k, v + x)
+
+
