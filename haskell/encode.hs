a229660936c97248723e311fa73990308dca8acf
michelebraidotti <michele.braidotti@gmail.com>
2013-05-30 00:56:13 +0200

    That's much better ...

--- haskell/encode.hs
+++ haskell/encode.hs
@@ -3,4 +3,9 @@ encode' :: (Num b) => [Char] -> [(Char, b)] -> [(Char, b)]
 
 encode [] = [('?', 0)]
 encode (x:xs) = encode' xs [(x, 1)]
-encode' (x:xs) y = encode' xs map (\x (k, v) -> if ( x == k ) then (k, v + 1) else (k, v)) y
+encode' [] res = res
+encode' (x:xs) res =
+	if ( x == fst ( last res ) ) 
+		then encode' xs ( init res ++ [(x, (snd (last res) + 1))] )
+		else encode' xs ( res ++ [(x,1)] )
+
