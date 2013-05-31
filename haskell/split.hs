fc87861f7cd4c55a671e55917ff2237acbfc0950
michele <michele.braidotti@gmail.com>
2013-05-31 08:28:58 -0700

    minor change ... still not working ...

--- haskell/split.hs
+++ haskell/split.hs
@@ -1,7 +1,9 @@
 split :: [a] -> Int -> ([a], [a])
+-- split' :: [a] -> Int -> ([a], [a]) -> ([a], [a])
+
 
 split x y = split' x y ([], [])
 split' (x:xs) y z 
-	| ( y > 0 ) 		= split' xs (y - 1) ((fst z) ++ [x], [])
 	| ( y > length (x:xs) )	= (y,[])
+	| ( y > 0 ) 		= split' xs (y - 1) ((fst z) ++ [x], [])
 	| otherwise 		= (fst z, x:xs)
