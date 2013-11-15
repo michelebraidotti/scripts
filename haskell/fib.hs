bddac70b2407f9c359804a5c0821b5b91f415511
michele <michele.braidotti@gmail.com>
2013-11-15 07:31:08 -0800

    fixed

--- haskell/fib.hs
+++ haskell/fib.hs
@@ -9,7 +9,9 @@ fibList 0 = [0]
 fibList 1 = [0,1]
 fibList n =  fibList (n - 1) ++ [fib n]
 
+-- what if instead of 1 rabbit per pair 
+-- we generate k rabbits per pair? ...
 fibExtra :: Int -> Int -> Int
 fibExtra 0 k = 0
-fibExtra 1 k = k
-fibExtra n k = fibExtra (n - 1) k + fibExtra (n - 2) k
+fibExtra 1 k = 1
+fibExtra n k = fibExtra (n - 1) k + k*(fibExtra (n - 2) k)
