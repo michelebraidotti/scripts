15062cc85ae43897635f231187a9f117ee1f7081
michele <michele@debian.(none)>
2014-09-12 16:49:13 +0200

    Even better Combinations!

--- haskell/combinations.hs
+++ haskell/combinations.hs
@@ -1,14 +1,5 @@
 combinations :: Int -> [a] -> [[a]]
-combinations 1 xs = convertListToListOfList xs
-combinations n xs
-	| length xs == n	= [xs]
-	| otherwise		= map ([head xs] ++) (combinations (n - 1) (tail xs)) ++ combinations n (tail xs)
-
-
-convertListToListOfList :: [a] -> [[a]]
-convertListToListOfList [] = [[]]
-convertListToListOfList (x:xs) =
-	if length xs == 0
-		then [[x]]
-		else [[x]] ++ convertListToListOfList xs
+combinations 0 _ = [[]]
+combinations _ [] = []
+combinations n xs = map ([head xs] ++) (combinations (n - 1) (tail xs)) ++ combinations n (tail xs)
 
