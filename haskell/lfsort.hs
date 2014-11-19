eb40359647da96ec3d8e0db57b9a496c61eca257
michele <michele@debian.(none)>
2014-11-19 15:54:36 +0100

    lfsort works! :-)

--- haskell/lfsort.hs
+++ haskell/lfsort.hs
@@ -1,12 +1,30 @@
 lfsort :: [[a]] -> [[a]]
 lfsort [] = []
-lfsort (x:xs) =  
+lfsort xs = map snd (lfsort' (computeFrequencies xs))
+
+lfsort' :: [(Int, [a])] -> [(Int, [a])]
+lfsort' [] = []
+lfsort' (x:xs) =
 	let 
-		lessFrequents = lfsort (lessFrequent x xs)
-		moreFrequents = lsort (moreFrequent x xs)
-	in
-		lessFrequents ++ [x] ++ moreFrequents
+	  smalls = lfsort' (filter (\y -> fst y <= fst x) xs)
+	  bigs = lfsort' (filter (\y -> fst y > fst x) xs)
+	in 
+	  smalls ++ [x] ++ bigs
 
-lessFrequent :: [a] -> [[a]] -> [[a]] -> [[a]]
 
-moreFrequent :: [a] -> [[a]] -> [[a]] -> [[a]]
+computeFrequencies :: [[a]] -> [(Int, [a])]
+computeFrequencies xs = computeFrequencies' xs xs
+
+computeFrequencies' :: [[a]] -> [[a]] -> [(Int, [a])]
+computeFrequencies' (x:xs) ys = 
+	if ( length xs == 0 ) 
+	then [((frequency x ys), x)]
+	else [((frequency x ys), x)] ++ (computeFrequencies' xs ys)
+
+
+frequency :: [a] -> [[a]] -> Int
+frequency _ [] = 0
+frequency x (y:ys) =
+	if (length x == length y) 
+	then 1 + (frequency x ys)
+	else 0 + (frequency x ys)
